f3f595906d31a89e5fb34d74c0a616862a71310a
Srikanth-varma <usrikanthvarma1996@gmail.com>
2025-11-03 16:14:49 +0530

    updated4 app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p style="color:red">Hello, World! deployed by srikanth</p>
+      <p style={{ color: "red" }}>Hello, World! deployed by srikanth</p>
     </div>
   );
 }
